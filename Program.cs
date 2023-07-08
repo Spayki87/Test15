c3ac784f8a1ff90c06e83cc8ecf5db0242aefe5c
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-08 23:24:08 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,77 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Консольное_меню
+namespace Степень_двойки
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            const string CommandYourName = "1";
-            const string CommandSetPassword = "2";
-            const string CommandChangeConsoleColor = "3";
-            const string CommandExit = "4";
+            Random random = new Random();
 
-            string userName;
-            string userPassword;
-            string userInput;
-            string colorRed = "1";
-            string colorBlye = "2";
-            string colorGreen = "3";
-            bool isExitProgram = true;
+            int namber = random.Next(0, 100);
+            int degree = 0;
+            int result = 1;
 
-            Console.WriteLine("Добро пожаловать в программу.");
-
-            while (isExitProgram)
+            while (result <= namber)
             {
-                Console.WriteLine(CommandYourName + " - введите свое имя.");
-                Console.WriteLine(CommandSetPassword + " - введите пароль.");
-                Console.WriteLine(CommandChangeConsoleColor + " - выберите текст консоли.");
-                Console.WriteLine(CommandExit + " - если хотите выйти из программы.");
-                userInput = Console.ReadLine();
-
-                switch (userInput)
-                {
-                    case CommandYourName:
-                        Console.WriteLine("Ввдите свое имя:");
-                        userName = Console.ReadLine();
-                        break;
-
-                    case CommandSetPassword:
-                        Console.WriteLine("Введите пароль:");
-                        userPassword = Console.ReadLine();
-                        break;
-
-                    case CommandChangeConsoleColor:
-                        Console.WriteLine("Выберите цвет консоли:");
-                        Console.WriteLine(colorRed + " - красный цвет.");
-                        Console.WriteLine(colorBlye + " - синий цвет.");
-                        Console.WriteLine(colorGreen + " - зеленый цвет");
-                        userInput = Console.ReadLine();
-
-                        if (userInput == colorRed)
-                        {
-                            Console.BackgroundColor = ConsoleColor.Red;
-                            Console.Clear();
-                        }
-                        else if (userInput == colorBlye)
-                        {
-                            Console.BackgroundColor = ConsoleColor.Blue;
-                            Console.Clear();
-                        }
-                        else if (userInput == colorGreen)
-                        {
-                            Console.BackgroundColor = ConsoleColor.Green;
-                            Console.Clear();
-                        }
-                        break;
-
-                    case CommandExit:
-                        isExitProgram = false;
-                        Console.WriteLine("Вы вышли из программы");
-                        break;
-                }
+                result *= 2;
+                degree++;
             }
+            Console.WriteLine("Рандомное число - " + namber);
+            Console.WriteLine("Минимальная степень двойки, превосходящая рандомное число - " + degree);
+            Console.WriteLine("Расчет примераж: 2 ^ " + degree + " = " + result);
         }
     }
 }
